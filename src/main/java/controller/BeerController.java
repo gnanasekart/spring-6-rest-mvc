5609ec03c009075b1501dcebddc91b33c7db5aeb
gnana sekar <gnanasekar69@hotmail.com>
2025-02-22 20:41:04 +0530

    Request mapping

--- src/main/java/controller/BeerController.java
+++ src/main/java/controller/BeerController.java
@@ -4,18 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import model.Beer;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import services.BeerService;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 public class BeerController {
     private final BeerService beerService;
 
-    @Qualifier("beerService")
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers(){
+        return beerService.listBeers();
+    }
+
     public Beer getBeerById(UUID uuid){
         log.debug("getBeerById from Controller");
         return beerService.getBeerById(uuid);
--- src/main/java/services/BeerService.java
+++ src/main/java/services/BeerService.java
@@ -2,9 +2,12 @@ package services;
 
 import model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+
+    List<Beer> listBeers();
 
     Beer getBeerById(UUID uuid);
 }
